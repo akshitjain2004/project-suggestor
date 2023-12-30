fc7693554d226f8fa1d6a3e1f589561b1542a9ea
Akshit Jain <akshitjain38630@gmail.com>
2023-12-30 22:11:06 +0530

    first commit

---
+++ src/components/Home.jsx
@@ -0,0 +1,28 @@
+import "./Home.css";
+const Home = () => {
+  return (
+  
+    <div className="home">
+      <div className="container">
+        <div className="domain">
+          <a className="btn">
+            <span> Web Development </span>
+          </a>
+          <a className="btn">
+            <span> Machine Learning </span>
+          </a>
+          <a className="btn">
+            <span> Artificial Intelligence </span>
+          </a>
+          <a className="btn">
+            <span> Data Structures </span>
+          </a>
+        </div>
+
+
+      </div>
+    </div>
+  );
+};
+
+export default Home;
